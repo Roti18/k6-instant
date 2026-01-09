753d27238dc45d2de34afe3c8e19213dc95b27d6
Roti18 <akuronif@gmail.com>
2026-01-10 00:24:33 +0700

    feat: add support for X-API-Key headers

--- utils/http.js
+++ utils/http.js
@@ -3,12 +3,13 @@ import { BASE_URL, API_KEY } from "./env.js";
 
 const headers = {
   "Content-Type": "application/json",
-  ...(API_KEY
-    ? {
-        Authorization: `Bearer ${API_KEY}`,
-      }
-    : {}),
 };
+
+if (API_KEY) {
+  headers["Authorization"] = `Bearer ${API_KEY}`;
+  headers["X-API-Key"] = API_KEY;
+  headers["x-api-key"] = API_KEY;
+}
 
 export const get = (path, params = {}) =>
   http.get(`${BASE_URL}${path}`, { headers, ...params });
