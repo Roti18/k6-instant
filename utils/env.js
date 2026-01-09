d17edf0248f056334f35be8dd5d4c9321186f55f
Roti18 <akuronif@gmail.com>
2026-01-10 01:15:23 +0700

    fix: env not deected

--- utils/env.js
+++ utils/env.js
@@ -1,5 +1,2 @@
-export const BASE_URL = __ENV.BASE_URL || "https://api.lament.ronxyz.xyz";
-export const API_KEY =
-  __ENV.API_KEY ||
-  "08fdc7cfebea02941fd37cd921c6b131d302a0dfc06fa84c777c9f20f79c2d43ef3b2553a7f4e46e26129a3100cc370d5c3491a2e9b74cbf6a4d32de7ffe6534c495c82ade1d99474c584bf566ae1a2b24118cb6b202b6fb4bcdb76f84876bc071e06ecd7118c59995158aed7e4bd4b33aad954132e94a856453647c243d7257";
-
+export const BASE_URL = __ENV.BASE_URL
+export const API_KEY = __ENV.API_KEY 
